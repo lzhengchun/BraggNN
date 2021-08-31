b0b3850999295dd90d8be543a934ac73ecc7c42b
Zhengchun Liu <zhengchun.liu@anl.gov>
2021-08-31 17:20:19 -0500

    adding logging module, used multiple workers for data aug

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from model import model_init, BraggNN
-import torch, argparse, os, time, sys, shutil
+import torch, argparse, os, time, sys, shutil, logging
 from util import str2bool, str2tuple, s2ituple
 from torch.utils.data import DataLoader
 from dataset import BraggNNDataset, load_val_dataset
@@ -14,7 +14,6 @@ parser.add_argument('-maxep',  type=int, default=500, help='max training epoches
 parser.add_argument('-fcsz',   type=s2ituple, default='16_8_4_2', help='size of dense layers')
 parser.add_argument('-psz',    type=int, default=11, help='working patch size')
 parser.add_argument('-aug',    type=int, default=1, help='augmentation size')
-parser.add_argument('-print',  type=str2bool, default=True, help='1:print to terminal; 0: redirect to file')
 
 args, unparsed = parser.parse_known_args()
 
@@ -31,14 +30,13 @@ if os.path.isdir(itr_out_dir):
     shutil.rmtree(itr_out_dir)
 os.mkdir(itr_out_dir) # to save temp output
 
-# redirect print to a file
-if args.print == 0:
-    sys.stdout = open(os.path.join(itr_out_dir, 'iter-prints.log'), 'w') 
+logging.basicConfig(filename=os.path.join(itr_out_dir, 'BraggNN.log'), level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 def main(args):
-    print("[Info] loading data into CPU memory, it will take a while ... ...")
+    logging.info("[Info] loading data into CPU memory, it will take a while ... ...")
     mb_data_iter = DataLoader(dataset=BraggNNDataset(psz=11, rnd_shift=args.aug), batch_size=args.mbsz, shuffle=True,\
-                              drop_last=True, pin_memory=True)
+                              num_workers=5, prefetch_factor=args.mbsz, drop_last=True, pin_memory=False)
 
     X_mb_val, y_mb_val = load_val_dataset(psz=args.psz, mbsz=None, rnd_shift=0, dev=torch_devs)
  
@@ -71,7 +69,7 @@ def main(args):
 
         _prints = '[Info] @ %.1f Epoch: %05d, loss: %.4f, elapse: %.2fms/epoch (computation=%.1fms/epoch, %.2f%%)' % (\
                    time.time(), epoch, args.psz * loss.cpu().detach().numpy(), time_e2e, time_comp, 100*time_comp/time_e2e)
-        print(_prints)
+        logging.info(_prints)
         with torch.no_grad():
             pred_val = model.forward(X_mb_val).cpu().numpy()            
 
@@ -80,11 +78,11 @@ def main(args):
         l2norm_train = np.sqrt((true_train[:,0] - pred_train[:,0])**2   + (true_train[:,1] - pred_train[:,1])**2) * args.psz
         l2norm_val   = np.sqrt((y_mb_val[:,0]   - pred_val[:,0])**2     + (y_mb_val[:,1]   - pred_val[:,1])**2)   * args.psz
 
-        print('[Train] @ %05d l2-norm of %5d samples: Avg.: %.4f, 50th: %.3f, 75th: %.3f, 95th: %.3f, 99.5th: %.3f (pixels).' % (\
-                (epoch, l2norm_train.shape[0], l2norm_train.mean()) + tuple(np.percentile(l2norm_train, (50, 75, 95, 99.5))) ) )
+        logging.info('[Train] @ %05d l2-norm of %5d samples: Avg.: %.4f, 50th: %.3f, 75th: %.3f, 95th: %.3f, 99.5th: %.3f (pixels).' % (\
+                     (epoch, l2norm_train.shape[0], l2norm_train.mean()) + tuple(np.percentile(l2norm_train, (50, 75, 95, 99.5))) ) )
 
-        print('[Valid] @ %05d l2-norm of %5d samples: Avg.: %.4f, 50th: %.3f, 75th: %.3f, 95th: %.3f, 99.5th: %.3f (pixels) \n' % (\
-                (epoch, l2norm_val.shape[0], l2norm_val.mean()) + tuple(np.percentile(l2norm_val, (50, 75, 95, 99.5))) ) )
+        logging.info('[Valid] @ %05d l2-norm of %5d samples: Avg.: %.4f, 50th: %.3f, 75th: %.3f, 95th: %.3f, 99.5th: %.3f (pixels) \n' % (\
+                     (epoch, l2norm_val.shape[0], l2norm_val.mean()) + tuple(np.percentile(l2norm_val, (50, 75, 95, 99.5))) ) )
 
         if torch.cuda.device_count() > 1:
             torch.save(model.module.state_dict(), "%s/mdl-it%05d.pth" % (itr_out_dir, epoch))
